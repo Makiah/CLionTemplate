58333efc6ca8daff9bd4c63c3f8b2d44f2e885ef
Makiah Bennett <makiahtbennett@gmail.com>
2019-10-18 15:54:57 -0400

    Add Qt support to CLion with googletest

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include <QApplication>
+#include <QPushButton>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+    QApplication app (argc, argv);
+
+    QPushButton button ("Hello world !");
+    button.show();
+
+    return app.exec();
 }
